eaa2392aebcdf03fd124424fc391fc5c3c2fe2af
koyuncuoglum95 <77508734+koyuncuoglum95@users.noreply.github.com>
2021-09-04 20:11:50 -0700

    BMI Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [result, setResult] = useState("");
+
+  const bmiNormalHandler = () => {
+  const heightConverter = parseFloat(height);
+  const weightConverter = parseInt(weight);
+  const bmi = (weightConverter) / (Math.pow(heightConverter,2));
+  const bmiConverter = parseFloat(bmi).toFixed(2);
+  setResult(bmiConverter);
+  console.log(bmiConverter);
+  var span = document.querySelector('span');
+  if(bmiConverter < 18.5 ) {
+    span.className="orange";
+  }
+  else if(bmiConverter > 18.5 && bmiConverter < 26 ){
+    span.className="green";
+  }
+  else if(bmiConverter > 24 && bmiConverter < 31) {
+    span.className="orangered";
+  }
+  else {
+    span.className= "red";
+  }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="bmiTitle">BMI Calculator</h1>
+      <label for="Weight" id="lblWeight">Weight(in kg)</label>
+      <input type="text" id="weight" placeholder="Enter weight" onChange={e => setWeight(e.target.value)}/>
+      <label for="Height" id="lblHeight">Height(in cm)</label>
+      <input type="text" id="height" placeholder="Enter height" onChange={e => setHeight(e.target.value)}/>
+      <button id="bmiCalculate" onClick={bmiNormalHandler}>Calculate</button>
+      <label id="nrmResult">Your BMI: <span>{result}</span></label>
+
+
+      <div id="underweight">
+        <p>UnderWeight Less than 18.5</p>
+      </div>
+
+      <div id="normalweight">
+        <p>Normal Weight 18.5-25</p>
+      </div>
+      <div id="overweight">
+        <p>Overweight</p>
+        <p id="up1">25-30</p>
+      </div>
+      <div id="obese">
+        <p>Obese More than 30</p>
+      </div>
+    
     </div>
   );
 }
